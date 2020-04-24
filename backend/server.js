8b2a03667c0c4890ab55d9e5471f6e6765759de2
Rachel <williamsrachelada@gmail.com>
2020-04-24 13:50:16 -0700

    Required mongoose, added variable to access db uri, established connection to mongoDB with mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+//helps connect to mongoDB database
+const mongoose = require('mongoose');
 
 //configures so we can have ENV variables in dotenv file
 require('dotenv').config();
@@ -12,6 +14,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 //middleware to allow us to parse json
 app.use(express.json());
+
+//the database uri for mongoDB
+const uri = process.env.ATLAS_URI;
+//starting connection to database.
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true} );
+const connection = mongoose.connection;
+//logs once connection is established
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully!");
+});
 
 //this line starts server and listens on certain port
 app.listen(port, () => {
